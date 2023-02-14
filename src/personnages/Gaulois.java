327e9a3a4a4b4b7336230d8eaa35f8d450d33245
bBibyk <125393249+bBibyk@users.noreply.github.com>
2023-02-14 19:31:46 +0100

    ajout gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.setNom(nom);
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public static void main(String[] args) {
+		
+	}
+}
